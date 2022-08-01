5292405bfd33f97f24d51ef4f289e97fbe560a84
黄雪朋 <huangxuepeng@chineyan.cn>
2022-08-01 19:34:08 +0800

    语言特性造成的问题

--- test.go
+++ test.go
@@ -1,46 +1,17 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"io"
-)
+import "strings"
 
-// 定义一个 Ustr 类型（以 string 为基类型）
-type Ustr string
-
-// 实现 Ustr 类型的 Read 方法
-func (s Ustr) Read(p []byte) (n int, err error) {
-	i, ls, lp := 0, len(s), len(p)
-	for ; i < ls && i < lp; i++ {
-		// 将小写字母转换为大写字母，然后写入 p 中
-		if s[i] >= 'a' && s[i] <= 'z' {
-			p[i] = s[i] + 'A' - 'a'
-		} else {
-			p[i] = s[i]
-		}
-<<<<<<< HEAD
-
-	}
-	arr = append(arr, pre)
-	return arr
-=======
-	}
-	// 根据读取的字节数设置返回值
-	switch i {
-	case lp:
-		return i, nil
-	case ls:
-		return i, io.EOF
-	default:
-		return i, errors.New("Read Fail")
-	}
-}
 func main() {
-	us := Ustr("hello world!")
-	buf := make([]byte, 32)
-	n, err := io.ReadFull(us, buf)
-	fmt.Println(string(buf))
-	fmt.Println(n, err)
->>>>>>> f3397ff6cdc0c9b0933a42b4dfa2f76392c688ff
+
+}
+
+func generateTheString(n int) string {
+	if n%2 == 0 {
+		a, b := strings.Repeat("a", n-1), strings.Repeat("b", 1)
+		return a + b
+	} else {
+		return strings.Repeat("a", n)
+	}
+	return ""
 }
