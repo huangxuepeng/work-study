54b8a7393eff7c36ed25c5996a90777a0b3e56e7
黄雪朋 <huangxuepeng@chineyan.cn>
2022-08-02 18:57:00 +0800

    新建第七周

--- test.go
+++ test.go
@@ -1,17 +1,12 @@
 package main
 
-import "strings"
+import (
+	"os/exec"
+	"sync"
+)
+
+var wg sync.WaitGroup
 
 func main() {
-
-}
-
-func generateTheString(n int) string {
-	if n%2 == 0 {
-		a, b := strings.Repeat("a", n-1), strings.Repeat("b", 1)
-		return a + b
-	} else {
-		return strings.Repeat("a", n)
-	}
-	return ""
+	ss := exec.Cmd()
 }
